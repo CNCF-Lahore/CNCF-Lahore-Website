3a30b0f4356e0d8fea94b6a86ebe811261e01bcf
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2024-02-14 16:01:16 +0500

    worked on ui fixings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className='mt-[64px]'>
             <Container>
                 <div className='flex justify-between items-center'>
                     <p className='text-[18px] text-white font-normal flex justify-start items-center gap-2'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg> hello@kcdlahore.com</p>
