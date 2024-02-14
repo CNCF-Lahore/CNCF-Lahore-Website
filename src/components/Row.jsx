90f6ae6b759b4b9e4529154d23c6dfd1aac65aff
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2024-02-14 05:11:30 +0500

    worked on ui enhancement

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -94,39 +94,47 @@ const Row = ({ title, data, rowID, className = "" }) => {
                   />
                   {contentToShow[index] && (
                     <div className="mt-2 p-[18px] opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-300">
-                      <div className="w-fit flex justify-between items-center gap-3">
-                        <button
-                          onClick={() => setModalDisplay(true)}
-                          className="rounded-full w-[32px] h-[32px] bg-white overflow-hidden flex justify-center items-center text-[12px] text-black"
-                        >
-                          <svg
-                            className=""
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="32"
-                            height="32"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="#000"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            class="lucide lucide-badge-info"
+                      <div className="flex justify-between items-center">
+                        <div className="w-fit flex justify-between items-center gap-3">
+                          <button
+                            onClick={() => setModalDisplay(true)}
+                            className="rounded-full w-[32px] h-[32px] bg-white overflow-hidden flex justify-center items-center text-[12px] text-black"
                           >
-                            <line x1="12" x2="12" y1="16" y2="12" />
-                            <line x1="12" x2="12.01" y1="8" y2="8" />
-                          </svg>
-                        </button>
+                            <svg
+                              className=""
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="32"
+                              height="32"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="#000"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              class="lucide lucide-badge-info"
+                            >
+                              <line x1="12" x2="12" y1="16" y2="12" />
+                              <line x1="12" x2="12.01" y1="8" y2="8" />
+                            </svg>
+                          </button>
+                          <button
+                            onClick={() => setModalDisplay(true)}
+                            className="rounded-full w-[32px] h-[32px] bg-[#222222] overflow-hidden flex justify-center items-center text-[12px] text-black border border-white"
+                          >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
+                          </button>
+                          <button
+                            onClick={() => setModalDisplay(true)}
+                            className="rounded-full w-[32px] h-[32px] bg-[#222222] overflow-hidden flex justify-center items-center text-[12px] text-black border border-white"
+                          >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-thumbs-up"><path d="M7 10v12"/><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z"/></svg>
+                          </button>
+                        </div>
                         <button
                           onClick={() => setModalDisplay(true)}
-                          className="rounded-full w-[32px] h-[32px] bg-[#222222] overflow-hidden flex justify-center items-center text-[12px] text-black border border-white"
+                          className="rounded-full w-[32px] h-[32px] bg-[#fff] overflow-hidden flex justify-center items-center text-[12px] text-black border border-white"
                         >
-                          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-                        </button>
-                        <button
-                          onClick={() => setModalDisplay(true)}
-                          className="rounded-full w-[32px] h-[32px] bg-[#222222] overflow-hidden flex justify-center items-center text-[12px] text-black border border-white"
-                        >
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-thumbs-up"><path d="M7 10v12"/><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z"/></svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
                         </button>
                       </div>
 
