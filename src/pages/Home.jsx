11f27652e7ada8e164f63030e76bfe610dbed4d6
Daud Irfan <daudirfanpakistan01@gmail.com>
2024-02-15 17:11:08 +0500

    NumberSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,8 @@ import Main from "../components/Main";
 import Row from "../components/Row";
 import Faq from "../components/faq";
 import Footer from "../components/Footer";
-
+import WhyJoin from "../components/WhyJoin";
+import NoSlide from "../components/NoSlide";
 // Mocks
 import speakers from "../mocks/speakers";
 import { tools } from "../mocks/tools";
@@ -15,11 +16,41 @@ const Home = () => {
   return (
     <>
       <Main />
-      <Row rowID="1" title="Our Popular Tools" data={tools} className="mt-[64px]" />
-      <Row rowID="1" title="Our Speakers" data={speakers} className="mt-[140px]" />
-      <Sponsors className="mt-[140px]" />
-      <Faq className="mt-[120px]" />
-      <Footer className="mt-[64px] md:mt-0" />
+      <Row
+        rowID="1"
+        title="Our Popular Tools"
+        data={[]}
+        className="mt-[90px]"
+      />
+      <WhyJoin />
+      <div id="our-speakers">
+        {/* <Row
+          rowID="2"
+          title="Our Speakers"
+          data={[]}
+          className="mt-[90px] speaker md:mt-[140px]"
+        /> */}
+        <NoSlide
+          rowID="2"
+          title="Our Speakers"
+          data={speakers}
+          className="mt-[90px] speaker md:mt-[140px] overflow-hidden"
+        />
+      </div>
+      <div id="event-agenda">
+        <Row
+          rowID="3"
+          title="Event Agenda"
+          data={[]}
+          className="mt-[90px] agenda md:mt-[140px]"
+        />
+      </div>
+      <div id="sponsors">
+        <Sponsors className="mt-[90px] md:mt-[140px]" />
+      </div>
+
+      <Faq className="mt-[90px] md:mt-[120px]" />
+      <Footer className="mt-[90px] md:mt-0" />
     </>
   );
 };
