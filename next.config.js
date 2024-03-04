8f84d9dff257fd74d6e317a5c41dfa51ddd2234b
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-03-04 21:56:32 +0000

    adding nuxtjs

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@
 const nextConfig = {
     basePath: '/CNCF-Lahore.github.io',
     assetPrefix: '/CNCF-Lahore.github.io/',
-    // If you have specific configurations for images, they should be defined here, not under "experimental"
-    images: {
-        // Example configuration - adjust according to your needs
-        domains: ['http://cncf-lahore.github.io/'],
-    },
-    // Ensure there are no configurations nested under "experimental" unless you're using features explicitly labeled as experimental by Next.js documentation
 };
 
 module.exports = nextConfig;
