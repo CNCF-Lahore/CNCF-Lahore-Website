16de88d857793818f549086b929ee00e7e9a81db
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-03-04 21:33:13 +0000

    Adding next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    // Assuming your GitHub Pages URL is "https://username.github.io/CNCF-Lahore.github.io"
+    // Set the basePath and assetPrefix to "/CNCF-Lahore.github.io"
+    basePath: '/CNCF-Lahore.github.io',
+    assetPrefix: '/CNCF-Lahore.github.io/',
 }
 
 export default nextConfig
