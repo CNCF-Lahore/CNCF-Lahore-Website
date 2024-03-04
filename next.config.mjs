1c84ae45d6cff8abf62bd392a83420258da9f9d0
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-03-04 21:45:34 +0000

    Adding config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 // next.config.mjs
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Adjust these settings to match your GitHub repository name
     basePath: '/CNCF-Lahore.github.io',
     assetPrefix: '/CNCF-Lahore.github.io/',
-    images: {
-        loader: 'akamai',
-        path: '',
-    },
-    // Additional configurations can go here
 };
 
 export default nextConfig;
