d3dc9b7e47229b2bcbe1f6b4b74a3989912f9fe1
Muhammad Daanyal <daanyalhere@gmail.com>
2024-03-06 01:24:16 +0500

    Updated meta.ts (meta data)

--- src/misc/meta.ts
+++ src/misc/meta.ts
@@ -1,13 +1,13 @@
 import { Metadata } from 'next'
 
 const common_meta: Metadata = {
-	applicationName: 'KCD Texas',
+	applicationName: 'KCD Lahore',
 	description: 'CNCF KCD Texas presents KCD Texas 2024 | Apr 12, 2024. Find event and ticket information.',
 	keywords: 'Cloud Native, Cloud Native community groups, Kubernetes Cloud Native meetup, Kubernetes meetup',
 	authors: [{ name: 'Zaid Bari', url: 'https://itszbari.com' }]
 }
 
 export const home_meta: Metadata = {
-	title: 'Home | KCD Texas',
+	title: 'Home | KCD Lahore',
 	...common_meta
 }
