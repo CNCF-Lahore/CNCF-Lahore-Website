cb3cfe8d2b32fe01d963232aff8a1464f7700667
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-03-06 18:46:38 +0000

    resolving issue

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    basePath: '/CNCF-Lahore.github.io',
-    assetPrefix: '/CNCF-Lahore.github.io/',
+
 };
 
 module.exports = nextConfig;
